e1b481dbda973800f273324bbb996ba59b5028b6
guoatty40 <69834365+guoatty40@users.noreply.github.com>
2020-09-22 11:52:17 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+    Project : If-else,while,loop,switch
+    Authorities : Mr.Sereephap Dangpatlaew
+    Date : 22/09/63
+*/
+
+#include<stdio.h>
+#include "broker.h"
+void main(){
+    printf("===Main Program==\n");
+    int mtqq();
+    //ifelse();
+    //grade();
+    grade2();
+    return 0;
+}
